a284448ed4cfc12ba497d74acc56a7b5c6c066e4
Tinotenda Mhedziso <tinomhedziso22@gmail.com>
2025-03-05 23:43:39 +0200

    Repo Variables fix

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,22 +1,26 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req) {
-  const clientId = process.env.GITHUB_CLIENT_ID;
-  const redirectUri = process.env.GITHUB_REDIRECT_URI;
+export async function GET(req: NextRequest) {
+	const clientId = process.env.GITHUB_CLIENT_ID;
+	const redirectUri = process.env.GITHUB_REDIRECT_URI;
 
-  // Get repoOwner and repoName from query params
-  const { repoOwner, repoName } = req.query;
+	// Get repoOwner and repoName from query parameters
+	const urlParams = req.nextUrl.searchParams;
+	const repoOwner = urlParams.get("repoOwner");
+	const repoName = urlParams.get("repoName");
 
-  // Ensure both repoOwner and repoName are provided
-  if (!repoOwner || !repoName) {
-    return NextResponse.json({ error: "Missing repoOwner or repoName" }, { status: 400 });
-  }
+	// Ensure both repoOwner and repoName are provided
+	if (!repoOwner || !repoName) {
+		return NextResponse.json(
+			{ error: "Missing repoOwner or repoName" },
+			{ status: 400 }
+		);
+	}
 
-  // Encode these parameters in the state to be passed to GitHub
-  const state = encodeURIComponent(JSON.stringify({ repoOwner, repoName }));
+	// Encode these parameters in the state to be passed to GitHub
+	const state = encodeURIComponent(JSON.stringify({ repoOwner, repoName }));
 
-  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=public_repo&state=${state}`;
+	const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=public_repo&state=${state}`;
 
-  return NextResponse.redirect(githubAuthUrl);
+	return NextResponse.redirect(githubAuthUrl);
 }
-
